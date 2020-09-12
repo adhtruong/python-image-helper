aeeab21551f2b0d936ab17fee0858df93ec42f3d
Andrew Truong <adh.truong@gmail.com>
2020-09-12 16:20:13 +0100

    Add setup file, update .gitignore

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+long_description = ""
+
+setuptools.setup(
+    name="cairo_helper",  # Replace with your own username
+    version="0.0.1",
+    author="Andrew Truong",
+    description="A package to help render images using Cairo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adhtruong/cairo_helper",
+    packages=setuptools.find_packages(),
+    install_requires=["pycairo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
