15e60669b01a127b6120dcc18613b908b43e9b0d
Andrew Truong <adh.truong@gmail.com>
2020-09-12 13:48:05 +0100

    Add models

---
+++ cairo-helper/__init__.py
@@ -0,0 +1,1 @@
+from models import Circle, Line, Point, Polygon
---
+++ cairo-helper/models.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import math
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Point:
+    x: float
+    y: float
+
+    def distance_squared(self, x: float, y: float) -> float:
+        return (self.x - x) ** 2 + (self.y - y) ** 2
+
+    def dist(self, other: Point) -> float:
+        return math.sqrt(self.distance_squared(other.x, other.y))
+
+    def __sub__(self, other: Point) -> Point:
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __add__(self, other: Point) -> Point:
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+
+@dataclass
+class Line:
+    start: Point
+    end: Point
+
+    def __add__(self, point: Point) -> Line:
+        return Line(self.start + point, self.end + point)
+
+    def __sub__(self, point: Point) -> Line:
+        return Line(self.start - point, self.end - point)
+
+
+@dataclass
+class Circle:
+    centre: Point
+    radius: float
+
+    def intersects(self, other: Circle) -> bool:
+        distance = self.centre.dist(other.centre)
+        return distance < self.radius + other.radius
+
+
+@dataclass
+class Polygon:
+    points: List[Point]
+
+    def get_centre(self) -> Point:
+        sum_point = sum(self.points, Point(0, 0))
+        return Point(sum_point.x / len(self.points), sum_point.y / len(self.points))
+
+    def __iter__(self):
+        return iter(self.points)
